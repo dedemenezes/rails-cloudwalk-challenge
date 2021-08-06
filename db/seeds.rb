616fd80c5e2da70b72a1cb621d6af06f9576f937
Andre Menezes <djdedemenezes@gmail.com>
2021-08-06 18:00:53 -0300

    seeding transaaction_amount in cents

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,13 @@ Transaction.destroy_all
 puts "db clean zo/"
 
 CSV.foreach(filepath, csv_options) do |row|
+  
   Transaction.create!(
     transaction_id: row['transaction_id'],
     merchant_id: row['merchant_id'],
     user_id: row['user_id'],
     card_number: row['card_number'],
-    transaction_date: row['transaction_date'],
+    transaction_date: row['transaction_date'] * 100,
     transaction_amount: row['transaction_amount'],
     device_id: row['device_id'],
     has_cbk:row['has_cbk'] 
