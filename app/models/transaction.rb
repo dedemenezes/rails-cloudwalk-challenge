8687fd5d8f06889824a095c662c5363ec7bcad72
Andre Menezes <djdedemenezes@gmail.com>
2021-08-07 21:37:43 -0300

    filtering method on model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,11 @@
 class Transaction < ApplicationRecord
   validates :transaction_id, :merchant_id, :user_id, :card_number, :transaction_date, :transaction_amount, presence: true
+
+  def self.chargebacks
+    where(has_cbk: true)
+  end
+
+  def self.visas
+    Transaction.where('card_number like ?', '4%')
+  end
 end
