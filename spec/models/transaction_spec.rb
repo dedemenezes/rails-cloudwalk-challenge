2d59df54069b4190a315fcad4de0cef2d1357c82
Andre Menezes <djdedemenezes@gmail.com>
2021-08-10 20:30:25 -0300

    added validations tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,28 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  subject { 
-    described_class.new(
-      transaction_id:21320398,
-      merchant_id: 29744,
-      user_id: 97051,
-      bin: 434505,
-      mid: 9116,
-      transaction_date: '2019-12-01T23:16:32.812632',
-      transaction_amount: 37456,
-      device_id: 285475,
-      has_cbk: false 
-    )
-  }
   context "validations" do
+    subject { 
+      described_class.new(
+        transaction_id:21320398,
+        merchant_id: 29744,
+        user_id: 97051,
+        bin: 434505,
+        mid: 9116,
+        transaction_date: '2019-12-01T23:16:32.812632',
+        transaction_amount: 37456,
+        device_id: 285475,
+        has_cbk: false 
+      )
+    }
     it 'is valid with valid attributes' do
-      expect { subject }.to be_valid
+      expect(subject).to be_valid
     end
-    it 'is not valid without transaction_id'
-    it 'is not valid without merchant_id'
-    it 'is not valid without user_id'
-    it 'is not valid without transaction_date'
-    it 'is not valid without transaction_amount'
+    it 'is not valid without transaction_id' do
+      subject.transaction_id = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid without merchant_id' do
+      subject.merchant_id = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid without user_id' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid without transaction_date' do
+      subject.transaction_date = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid without transaction_amount' do
+      subject.transaction_amount = nil
+      expect(subject).to_not be_valid
+    end
   end
   
 end
