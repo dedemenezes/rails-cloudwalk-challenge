98eb93106a3565dda72c89b3335a23cff7c601f2
Andre Menezes <djdedemenezes@gmail.com>
2021-08-10 20:25:32 -0300

    rspec:install

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { 
+    described_class.new(
+      transaction_id:21320398,
+      merchant_id: 29744,
+      user_id: 97051,
+      bin: 434505,
+      mid: 9116,
+      transaction_date: '2019-12-01T23:16:32.812632',
+      transaction_amount: 37456,
+      device_id: 285475,
+      has_cbk: false 
+    )
+  }
+  context "validations" do
+    it 'is valid with valid attributes' do
+      expect { subject }.to be_valid
+    end
+    it 'is not valid without transaction_id'
+    it 'is not valid without merchant_id'
+    it 'is not valid without user_id'
+    it 'is not valid without transaction_date'
+    it 'is not valid without transaction_amount'
+  end
+  
 end
