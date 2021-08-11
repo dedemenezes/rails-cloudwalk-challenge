19cc85c1562a5d6da48783be312269df4ffcd309
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 20:06:37 -0300

    removed save instace from set transformations methdos and now we are saving when seed andwhen crete on controller

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 class Transaction < ApplicationRecord
   validates :transaction_id, :merchant_id, :user_id, :transaction_date, :transaction_amount, presence: true
+  validates :transaction_id, uniqueness: true
 
   def transform_datetime
     is_weekend
@@ -42,7 +43,6 @@ class Transaction < ApplicationRecord
     self.user_avg_amount_1Day_window = user_behaviours["1"][:avg_amount]
     self.user_avg_amount_7Day_window = user_behaviours["7"][:avg_amount]
     self.user_avg_amount_30Day_window = user_behaviours["30"][:avg_amount]
-    save!
   end
 
   def set_merchant_risk_windows(merchant_info_risk)
@@ -52,18 +52,15 @@ class Transaction < ApplicationRecord
     self.merchant_nb_tx_1day_window = merchant_info_risk["1"][:window_transactions]
     self.merchant_nb_tx_7day_window = merchant_info_risk["7"][:window_transactions]
     self.merchant_nb_tx_30day_window = merchant_info_risk["30"][:window_transactions]
-    save!
   end
 
   def shop_time
     night_time = transaction_date.hour < 7
     night_time ? self.during_night = 1 : self.during_night = 0
-    save!
   end
 
   def is_weekend
     transaction_date.on_weekend? ? self.during_weekend = 1 : self.during_weekend = 0 
-    save!
   end
 
   def self.save_to_csv
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ whole_seed_time = Time.now
 
 puts "cleaning db"
 Transaction.destroy_all
-
 puts "db clean zo/"
 
 puts "generating transactions"
 start_time = Time.now
+puts start_time
 CSV.foreach(filepath, csv_options) do |row|
   
   Transaction.create!(
@@ -38,21 +38,30 @@ puts"Generated #{Transaction.count} transaction(s)\nTime elapsed: #{Time.now - s
 
 puts "Transforming datetime"
 start_time = Time.now
-Transaction.all.each { |transaction| transaction.transform_datetime }
+Transaction.all.each do |transaction|
+  transaction.transform_datetime
+  transaction.save!
+end
 
 puts "#{Transaction.all.order(:transaction_date).first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
 
 puts "Transforming user id"
 start_time = Time.now
-Transaction.all.each { |transaction| transaction.transform_user_id }
+Transaction.all.each do |transaction|
+  transaction.transform_user_id
+  transaction.save!
+end
 
 puts "#{Transaction.all.order(:transaction_date).first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
 
 puts "Transforming merchant id. Setting merchant risk..."
 start_time = Time.now
-Transaction.all.each { |transaction| transaction.transform_merchant_id }
+Transaction.all.each do |transaction|
+  transaction.transform_merchant_id
+  transaction.save!
+end
 
 puts "#{Transaction.all.order(:transaction_date).first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
 puts "----------"
-puts "Whole seed took #{Time.now - whole_seed_time} seconds"
+puts "Whole seed took #{Time.now - whole_seed_time} seconds\nStored #{Transaction.count} transactions"
 puts "zo/"
