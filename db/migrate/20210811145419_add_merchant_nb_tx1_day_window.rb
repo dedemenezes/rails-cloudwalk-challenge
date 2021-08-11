da3175410b36b97ba5dd5f854d12ff5c5779a75d
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 12:02:00 -0300

    added others window periods on AddMerchantNbTx1DayWindow migration

--- db/migrate/20210811145419_add_merchant_nb_tx1_day_window.rb
+++ db/migrate/20210811145419_add_merchant_nb_tx1_day_window.rb
@@ -1,5 +1,7 @@
 class AddMerchantNbTx1DayWindow < ActiveRecord::Migration[6.1]
   def change
     add_column :transactions, :merchant_nb_tx_1day_window, :integer
+    add_column :transactions, :merchant_nb_tx_7day_window, :integer
+    add_column :transactions, :merchant_nb_tx_30day_window, :integer
   end
 end
