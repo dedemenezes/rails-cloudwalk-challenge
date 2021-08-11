2008b68b92b53f043fd6084d95a98c7bda4168f1
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 11:43:05 -0300

    getting merchant risk and merchant number of transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -45,21 +45,48 @@ class Transaction < ApplicationRecord
     users_ids
   end
 
-  def get_merchant_risk(delay_period=7, window_size_days=[1,7,30])
+  def get_merchant_risk(delay_period=6, window_size_days=[1,7,30])
     start_date = Date.new(2019,11,01)
+    end_date = Date.new(2019,12,01)
     id = merchant_id
+    merchant_risk = {}
     # Delay period
     range_delay = ((start_date.beginning_of_day)..(start_date + delay_period.day).end_of_day)
-    transactions_delay_period = Transaction.where(transaction_date: range_delay).order(:transaction_date)
-    fraudulent_transactions_delay_period = transactions_delay_period.where(has_cbk: true)
-    
+    # Delay period all transactions
+    merchant_transactions_delay_period = Transaction.where(merchant_id: id, transaction_date: range_delay).order(:transaction_date)
+    # Delay period fraudulent transactions
+    merchant_fraudulent_transactions_delay_period = merchant_transactions_delay_period.where(has_cbk: true)
+
+    # Window period
     window_size_days.each do |window_size|
       # TODO
-      binding.pry
+      range_window = ((start_date.beginning_of_day)..(start_date + delay_period.day + window_size.day).end_of_day)
+      if (start_date + delay_period.day + window_size.day).end_of_day > end_date.end_of_day
+        range_window = ((start_date.beginning_of_day)..(end_date.end_of_day))
+      end
+      # Window period all transactions
+      merchant_transactions_delay_window_period = Transaction.where(merchant_id: id, transaction_date: range_window).order(:transaction_date)
+      # Window period fradulent transactions
+      merchant_fraudulent_transactions_delay_window_period = merchant_transactions_delay_window_period.where(has_cbk: true)
+      #Check if exist any transaction within window period
+      if merchant_transactions_delay_window_period.size > 0
+        merchant_transactions_window_period = merchant_transactions_delay_window_period.count - merchant_transactions_delay_period.count
+        merchant_fraudulent_transactions_window_period = merchant_fraudulent_transactions_delay_window_period.count - merchant_fraudulent_transactions_delay_period.count
+        merchant_risk[window_size.to_s] = {
+          merchant_risk: (merchant_fraudulent_transactions_window_period.to_f / merchant_transactions_window_period).round(4),
+          window_transactions: merchant_transactions_window_period
+        }
+      else
+        merchant_risk[window_size.to_s] = {
+          merchant_risk: 0.to_f,
+          window_transactions: 0
+        } 
+      end
     end
-    merchant_transactions = Transaction.where(merchant_id: id).order(:transaction_date)
-    merchant_transactions.where(has_cbk: true).order(:transaction_date)
+    binding.pry
+    merchant_risk
   end
+
   private
 
   def daily_window(user_behaviour, customer_transactions)
