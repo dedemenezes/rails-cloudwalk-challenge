5cfa33a907d51a91add5610953cd61718b10cacf
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 06:01:13 -0300

    can define user behaviour using user_spending_behaviour and passing the user_id

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -18,72 +18,84 @@ class Transaction < ApplicationRecord
     shop_time
   end
 
-  def self.get_users_transactions
-    users_transactions = {}
-    all.each do |t|
-      unless users_transactions["#{t.user_id}"]
-        users_transactions["#{t.user_id}"] = Transaction.where(user_id: t.user_id)
-      end
-    end
+  # Can be useful to call directly from user_id
+  # def self.users_behaviours
+  #   get_users_ids.each do |user_id|
+  #     user_spending_behaviour(user_id)
+  #   end
+  # end
+  # def self.get_users_ids
+  #   users_ids = []
+  #   all.each do |t|
+  #     unless users_ids.include? t.user_id
+  #       users_ids << t.user_id
+  #     end
+  #   end
+  #   users_ids
+  # end
 
-    users_transactions
-  end
-
-  def self.spending_behaviour(customer_id, window_size_days=[1,7,30])
+  def user_spending_behaviour(customer_id, window_size_days=[1,7,30])
     # find user transactions and order by transaction date
-    customer_transactions = Transaction.where(user_id: customer_id[:user_id]).order(:transaction_date)
-    binding.pry
+    customer_transactions = Transaction.where(user_id: customer_id).order(:transaction_date)
     # loop over windows and set number of transactions by customer and avg amount spent on each window
     start_date = Date.new(2019,11,01)
-    window_size_days.each_with_index do |days, index|
-      range = ((start_date.beginning_of_day)..((start_date + days.day).end_of_day))
-      range_transactions = customer_transactions.where(transaction_date: range)
-      number_of_transactions = range_transactions.count
-      total_amount_spent = range_transactions.sum(:transaction_amount)
-      if number_of_transactions > 0
-        avg_spent_amount = total_amount_spent / number_of_transactions 
-      else
-        avg_spent_amount = 0
+    window_size_days.each do |window_days|
+      # Create user behavuour for each window days
+      user_behaviour = set_window_behaviour(start_date, window_days, customer_transactions)
+      # Associate behaviour from window to user transactions
+      case window_days
+      when 1
+        daily_window(user_behaviour, customer_transactions)
+      when 7
+        weekly_window(user_behaviour, customer_transactions)
+      when 30
+        monthly_window(user_behaviour, customer_transactions)
       end
-      range_transactions.each do |t|
-        t.set_avg_amount(days, avg_spent_amount)
-        t.set_number_of_transactions(days,number_of_transactions)
-      end
-      binding.pry
     end
-    # return class Transaction
   end
 
-  def set_number_of_transactions(days,number_of_transactions)
-    case days
-    when 1
-      self.user_nb_tx_1Day_window = number_of_transactions
-    when 7
-      self.user_nb_tx_7Day_window = number_of_transactions
-    when 30
-      self.user_nb_tx_30Day_window = number_of_transactions
-    else
-      puts "oh diachu"
-    end
-    save!
+  def set_window_behaviour(start_date, window, transactions)
+    range = ((start_date.beginning_of_day)..((start_date + window.day).end_of_day))
+    range_transactions = transactions.where(transaction_date: range)
+    range_transactions.present? ? get_window_behaviour(range_transactions) : get_window_behaviour(0)
   end
-  
-  def set_avg_amount(days, amount)
-    case days
-    when 1
-      self.user_avg_amount_1Day_window = amount
-    when 7
-      self.user_avg_amount_7Day_window = amount
-    when 30
-      self.user_avg_amount_30Day_window = amount
+
+  def get_window_behaviour(transactions)
+    if transactions == 0 
+      { avg_amount: 0, nbr_transactions: 0 }
     else
-      puts "oh diachu"
+      { avg_amount: transactions.sum(:transaction_amount) / transactions.count,
+        nbr_transactions: transactions.count 
+      }  
     end
-    save!
   end
 
   private
-  
+
+  def daily_window(user_behaviour, customer_transactions)
+    customer_transactions.each do |t|
+      t.user_nb_tx_1Day_window = user_behaviour[:nbr_transactions]
+      t.user_avg_amount_1Day_window = user_behaviour[:avg_amount]
+      t.save!
+    end
+  end
+
+  def weekly_window(user_behaviour, customer_transactions)
+    customer_transactions.each do |t|
+      t.user_nb_tx_7Day_window = user_behaviour[:nbr_transactions]
+      t.user_avg_amount_7Day_window = user_behaviour[:avg_amount]
+      t.save!
+    end
+  end
+
+  def monthly_window(user_behaviour, customer_transactions)
+    customer_transactions.each do |t|
+      t.user_nb_tx_30Day_window = user_behaviour[:nbr_transactions]
+      t.user_avg_amount_30Day_window = user_behaviour[:avg_amount]
+      t.save!
+    end
+  end
+
   def shop_time
     night_time = transaction_date.hour < 7
     night_time ? self.during_night = 1 : self.during_night = 0
