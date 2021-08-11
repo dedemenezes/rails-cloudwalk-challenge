a0385b0b34e9524062f9a47135ca4c2f65e694d9
Andre Menezes <djdedemenezes@gmail.com>
2021-08-10 21:48:06 -0300

    added class method to transform datetime data on all transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -13,34 +13,25 @@ class Transaction < ApplicationRecord
     Transaction.where('card_number like ?', '5%')
   end
 
-  def weekend
-    is_weekend
-    during_weekend
-  end
-
-  def night_shop
-    shop_time
-    during_night
+  def self.transform_datetime
+    start = Time.now
+    all.each do |t|
+      t.is_weekend
+      t.shop_time
+    end
+    puts Time.now - start
   end
 
   private
 
   def shop_time
     night_time = transaction_date.hour < 7
-    if night_time
-      self.during_night = 1
-    else
-      self.during_night = 0
-    end
+    night_time ? self.during_night = 1 : self.during_night = 0
     save!
   end
 
   def is_weekend
-    if transaction_date.on_weekend?
-      self.during_weekend = 1
-    else
-      self.during_weekend = 0 
-    end
+    transaction_date.on_weekend? ? self.during_weekend = 1 : self.during_weekend = 0 
     save!
   end
 end
