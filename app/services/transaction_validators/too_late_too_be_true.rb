c3699ec5571ffb5c659837ab5026b9899001366d
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 17:31:15 -0300

    removed binding.pry

--- app/services/transaction_validators/too_late_too_be_true.rb
+++ app/services/transaction_validators/too_late_too_be_true.rb
@@ -3,7 +3,6 @@ module TransactionValidators
     def self.above_limit(transaction)
       usr_monthly_avg_amount = Transaction.find_by(user_id: transaction.user_id).user_avg_amount_30Day_window
       transaction.transform_datetime
-      binding.pry
       if transaction.during_night == 1 && transaction.during_weekend == 1
         transaction.transaction_amount <= usr_monthly_avg_amount + 25
       elsif transaction.during_night == 1
