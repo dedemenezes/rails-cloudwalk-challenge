61b052dee2939b86f128d523b069674a34bda980
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 12:47:31 -0300

    seeding and storing merchant_risk window info

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,10 +39,16 @@ puts "Transforming datetime"
 start_time = Time.now
 Transaction.all.each { |transaction| transaction.transform_datetime }
 
-puts "#{Transaction.first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
+puts "#{Transaction.all.order(:transaction_date).first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
 
 puts "Transforming user id"
 start_time = Time.now
 Transaction.all.each { |transaction| transaction.transform_user_id }
 
-puts "#{Transaction.first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
+puts "#{Transaction.all.order(:transaction_date).first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
+
+puts "Transforming merchant id. Setting merchant risk..."
+start_time = Time.now
+Transaction.all.each { |transaction| transaction.transform_merchant_id }
+
+puts "#{Transaction.all.order(:transaction_date).first.inspect}\nTime elapsed: #{Time.now - start_time} seconds"
