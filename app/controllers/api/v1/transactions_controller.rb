f78d3bbe92b402960adecb1b6dc417332dfa8163
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 16:17:18 -0300

    called module TransactionValidator to check for lmiit and time

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -2,12 +2,18 @@ class Api::V1::TransactionsController < Api::V1::BaseController
 
   def check
     @transaction = Transaction.new(transaction_params)
+    limit_valid = TransactionValidators::TooLateTooBeTrue.above_limit(@transaction)
     binding.pry
   end
+
+  # First score will vary from 0 to 1
+  # Mechant risk will be counted as 1/3
+  # during_night counted as 1/3
+  # avg amount 1/3
 
   private
 
   def transaction_params
-    params.require(:transaction).permit(:transaction_id, :merchant_id, :user_id, :card_number, :transaction_date, :transaction_amount, :device_id)
+    params.require(:transaction).permit(:transaction_id, :merchant_id, :user_id, :bin, :mid, :transaction_date, :transaction_amount, :device_id)
   end
 end
