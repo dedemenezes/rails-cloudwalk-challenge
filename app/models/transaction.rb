f0e2caf3bc4415ec34b7f1f361c426d2d815025c
Andre Menezes <djdedemenezes@gmail.com>
2021-08-11 03:07:24 -0300

    can get avg amount and number of transactions during period and save it to the db

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -18,8 +18,72 @@ class Transaction < ApplicationRecord
     shop_time
   end
 
-  private
+  def self.get_users_transactions
+    users_transactions = {}
+    all.each do |t|
+      unless users_transactions["#{t.user_id}"]
+        users_transactions["#{t.user_id}"] = Transaction.where(user_id: t.user_id)
+      end
+    end
 
+    users_transactions
+  end
+
+  def self.spending_behaviour(customer_id, window_size_days=[1,7,30])
+    # find user transactions and order by transaction date
+    customer_transactions = Transaction.where(user_id: customer_id[:user_id]).order(:transaction_date)
+    binding.pry
+    # loop over windows and set number of transactions by customer and avg amount spent on each window
+    start_date = Date.new(2019,11,01)
+    window_size_days.each_with_index do |days, index|
+      range = ((start_date.beginning_of_day)..((start_date + days.day).end_of_day))
+      range_transactions = customer_transactions.where(transaction_date: range)
+      number_of_transactions = range_transactions.count
+      total_amount_spent = range_transactions.sum(:transaction_amount)
+      if number_of_transactions > 0
+        avg_spent_amount = total_amount_spent / number_of_transactions 
+      else
+        avg_spent_amount = 0
+      end
+      range_transactions.each do |t|
+        t.set_avg_amount(days, avg_spent_amount)
+        t.set_number_of_transactions(days,number_of_transactions)
+      end
+      binding.pry
+    end
+    # return class Transaction
+  end
+
+  def set_number_of_transactions(days,number_of_transactions)
+    case days
+    when 1
+      self.user_nb_tx_1Day_window = number_of_transactions
+    when 7
+      self.user_nb_tx_7Day_window = number_of_transactions
+    when 30
+      self.user_nb_tx_30Day_window = number_of_transactions
+    else
+      puts "oh diachu"
+    end
+    save!
+  end
+  
+  def set_avg_amount(days, amount)
+    case days
+    when 1
+      self.user_avg_amount_1Day_window = amount
+    when 7
+      self.user_avg_amount_7Day_window = amount
+    when 30
+      self.user_avg_amount_30Day_window = amount
+    else
+      puts "oh diachu"
+    end
+    save!
+  end
+
+  private
+  
   def shop_time
     night_time = transaction_date.hour < 7
     night_time ? self.during_night = 1 : self.during_night = 0
